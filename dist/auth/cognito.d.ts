55f9b7e66f6f8899ba27fe4d7d124a73f5a6b82f
Konstantin Romanenko <kromanenko@lenovo.com>
2022-07-24 15:24:17 +0300

    update version to 0.4.10

---
+++ dist/auth/cognito.d.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react';
+export declare const CognitoAuth: import("@aws-amplify/auth/lib-esm/Auth").AuthClass;
+export declare const CognitoAuthProvider: ({ children }: {
+    children: ReactNode;
+}) => JSX.Element;
+export declare const listenForAuth: () => void;

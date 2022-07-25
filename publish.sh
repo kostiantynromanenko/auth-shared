331ba15d9a02584481e5ff8dd352109ddb438258
Konstantin Romanenko <kromanenko@lenovo.com>
2022-07-25 22:04:35 +0300

    new changes

--- publish.sh
+++ publish.sh
@@ -5,7 +5,6 @@ PACKAGE_VERSION=$(npm show . version)
 npm run build
 git add .
 git commit -m "update version to $PACKAGE_VERSION"
-git tag -a "v$PACKAGE_VERSION" -m "version $PACKAGE_VERSION"
 git push origin
 npm publish
 
